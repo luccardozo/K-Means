2ec9eb82f08e42f720ed2282cf9735057fc45ecd
FBachini <43426398+FBachini@users.noreply.github.com>
2021-05-09 00:38:23 -0300

    Add elbow plots

--- kmeans.py
+++ kmeans.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import random as rd
 
@@ -15,37 +14,39 @@ def init_centroids(x, k):
     for i in range(0, k):
         # Garante que não vamos adicionar dois centroides no mesmo lugar inicial. Gerando sempre um novo aleatório.
         while len(centroids.drop_duplicates()) != i + 1:
-            rand = rd.randint(0, x.shape[0]) #valor aleatorio entre zero e o numero de linhas (valores de entrada)
+            rand = rd.randint(0, x.shape[0])  # valor aleatorio entre zero e o numero de linhas (valores de entrada)
             centroids = centroids.append(x.iloc[rand])
 
     return centroids
 
 
-def kmeans(d, k, max_iterations=10, decimal_precision=3):
+def kmeans(d, k):
     """
     :param d: Dataset
     :param k:  K-Clusters
 
     :return: Clusters
     """
+    max_iterations = 10
+    decimal_precision = 3
     # Escolhe centroides aleatorios não iguais
     global clusters
     centroids = init_centroids(d, k)
     centroids.astype(float).round(decimals=decimal_precision)
-    m = d.shape[0] #numero de linhas
+    m = d.shape[0]  # numero de linhas
     changed = True
     curr_iteration = 0
     # Calcula distancia Euclideanea
-    while ((max_iterations > curr_iteration) and changed):
+    while (max_iterations > curr_iteration) or changed:
         curr_iteration += 1
         distance = pd.DataFrame()
         for i in range(k):
-            #Serie com a distancia calculada para cada um dos pontos.
+            # Serie com a distancia calculada para cada um dos pontos.
             tempDist = np.sum((d - centroids.iloc[i]) ** 2, axis=1)
-            #Matriz com a distancia de cada centroide para cada ponto.
+            # Matriz com a distancia de cada centroide para cada ponto.
             distance = pd.concat([distance, tempDist], axis=1, ignore_index=True)
 
-        #Cada ponte aponta pra o cluster que pertence
+        # Cada ponte aponta pra o cluster que pertence
         min_dist = distance.idxmin(axis='columns')
 
         # Separa os clusters e adiciona as linhas no dicionario
@@ -69,17 +70,12 @@ def kmeans(d, k, max_iterations=10, decimal_precision=3):
     return clusters, curr_iteration, WCSS(k, clusters, centroids)
 
 
-def WCSS(k, clusters, centroids):
+def WCSS(k, c, centroids):
     wcss = 0
-    for i in range(k):
-        wcss += np.sum((clusters[i] - centroids.iloc[i]) ** 2)
+    for j in range(k):
+        wcss += np.sum((c[j] - centroids.iloc[j]) ** 2)
     return wcss.mean()
 
-
-dataset1 = cat_to_num(pd.read_csv('data\SocioDemographic_Vars.txt', sep='\t'), ["Gender", "Only.child", "Education"])
-dataset = pd.read_csv('data\Phobias_Vars.txt', sep='\t')
-
-print(kmeans(dataset, 4))
 
 """"
 EVITANDO PERDER OS TRABALHOS
@@ -89,6 +85,5 @@ if dtype == 'string':
 
 if dtype == 'string':
     centroids = centroids.append(clusters[i].mode(axis=0).iloc[0], ignore_index=True)
-    
     
 """
--- main.py
+++ main.py
@@ -1,4 +1,36 @@
-import pandas as pd
-import time
+from kmeans import *
+import matplotlib.pyplot as plt
 
-dataset = pd.read_csv('data\Phobias_Vars.txt', sep='\t')
+data = {'hobbies': pd.read_csv('data\HobbiesAndInterests_Vars.txt', sep='\t'),
+        'music': pd.read_csv('data\MusicAndMovies_Vars.txt', sep='\t'),
+        'person': pd.read_csv('data\Personality_Vars.txt', sep='\t'),
+        'phobia': pd.read_csv('data\Phobias_Vars.txt', sep='\t'),
+        'socio': cat_to_num(pd.read_csv('data\SocioDemographic_Vars.txt', sep='\t'),
+                            ["Gender", "Only.child", "Education"]),
+        'spend': pd.read_csv('data\SpendingHabits_Vars.txt', sep='\t')}
+
+"""
+wcss_array = {'hobbies': np.array([]),
+        'music': np.array([]),
+        'person': np.array([]),
+        'phobia': np.array([]),
+        'socio': np.array([]),
+        'spend': np.array([])} 
+"""
+
+wcss_array = np.array([])
+
+for i in range(1, 12):
+    """
+    for key, dset in data.items():
+        cl, c_iter[key], wcss_array[key] = kmeans(dset, i)
+    """
+    cl, c_iter, wcss = kmeans(data['hobbies'], i)  # DEMORA HORRORES
+    wcss_array = np.append(wcss_array, wcss)
+
+k_array = np.arange(1, 12, 1)
+plt.plot(k_array, wcss_array)
+plt.xlabel('Clusters')
+plt.ylabel('WCSS')
+plt.title('Elbow Method')
+plt.show()
